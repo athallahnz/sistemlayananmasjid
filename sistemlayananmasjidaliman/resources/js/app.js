583bb6cd25e1d384b2095626796a612e8dc22e36
athallahnz <athallahnz57@gmail.com>
2024-06-03 20:08:25 +0700

    add laravel

--- sistemlayananmasjidaliman/resources/js/app.js
+++
@@ -1,9 +0,0 @@
-import "./bootstrap";
-import.meta.glob(["../images/**"]);
-
-function getJadwalSholat(){
-    fetch('https://api.banghasan.com/sholat/format/json/jadwal/kota/703/tanggal/2017-02-07')
-    .then(response => console.log(response))
-}
-
-getJadwalSholat();
